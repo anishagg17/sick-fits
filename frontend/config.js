d5b20e432aa572a27c815b39f3de632ad270adff
Anish Aggarwal <43617894+anishagg17@users.noreply.github.com>
2020-01-12 01:43:53 +0530

    modified yoga endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://sick-fits-live-2ab31883c8.herokuapp.com/sick-fits-live/prod`;
+export const prodEndpoint = `https://sick-fits-live.herokuapp.com/`;
 export const perPage = 4;
